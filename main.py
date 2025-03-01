56c0eafd7a7ee5f1cb10c9503a4f4c5d134c8345
byrdstephenr <137971615+byrdstephenr@users.noreply.github.com>
2025-03-01 08:25:10 -0700

    Update main.py basic_cleaning

--- main.py
+++ main.py
@@ -57,9 +57,9 @@ def go(config: DictConfig):
          env_manager="local",
          parameters={
              "input_artifact": "sample.csv:latest",
-             "output_artifact_name": "clean_sample.csv",
-             "output_artifact_type": "clean_sample",
-             "output_artifact_description": "Data with outliers and null values removed",
+             "output_artifact_name": "clean_sample1.csv",
+             "output_artifact_type": "clean_data",
+             "output_artifact_description": "Cleaned dataset with outliers removed",
              "min_price": config['etl']['min_price'],
              "max_price": config['etl']['max_price']
          },
